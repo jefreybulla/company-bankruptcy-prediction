32e567863458e8c07876a7cd24aff909173ba733
adapaania <77148279+adapaania@users.noreply.github.com>
2026-05-04 09:26:26 -0400

    Updated notebooks and added Phase 3 generalization files (#14)
    - Made modifications to my cluster notebooks for consistency and improvements
    - Added final Phase 3 files (CSV + notebook)
    - Made a separate folder for the final files, named  '4_project'.
    - Verified that outputs are present in all ipynb files across clusters
    - Report is still pending and will be added next
    
    Co-authored-by: “adapaania” <“aaniadap”@gmail.com”>

--- phase-2/cluster-10/cluster_10.py
+++
@@ -1,9 +0,0 @@
-# Example model file. 
-# Create a new file in this folder using one notebook per cluster
-
-# In your cluster foler please add 
-# - your notebook file
-# - your py file (exported from the notebook) so we can review yout code on Github
-# - your joblib file that stores your preprocessing and fitted model
-
-# Before you open your PR, test your joblib file. See example in phase-3/joblib_usage.ipynb
